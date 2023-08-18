adfda18926df2e688a5975a03d83b529cf3bbf3e
enoch <enokkang@gmail.com>
2023-08-18 12:53:32 -0500

    adds form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
   state = {
     characters: [
-      {
-        name: "Charlie",
-        job: "Janitor",
-      },
-      {
-        name: "Mac",
-        job: "Bouncer",
-      },
-      {
-        name: "Dee",
-        job: "Aspring actress",
-      },
-      {
-        name: "Dennis",
-        job: "Bartender",
-      },
+      // {
+      //   name: "Charlie",
+      //   job: "Janitor",
+      // },
+      // {
+      //   name: "Mac",
+      //   job: "Bouncer",
+      // },
+      // {
+      //   name: "Dee",
+      //   job: "Aspring actress",
+      // },
+      // {
+      //   name: "Dennis",
+      //   job: "Bartender",
+      // },
     ],
   };
 
@@ -33,12 +34,17 @@ class App extends Component {
     });
   };
 
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
   render() {
     const { characters } = this.state;
 
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     );
   }
